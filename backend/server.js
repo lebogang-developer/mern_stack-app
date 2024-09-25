08f39245a75ceb10f3fb50e470faf622005adb93
Lebogang Sekaleli <lebogangsekaleli@gmail.com>
2024-09-25 11:23:06 +0200

    Connected to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { connectDB } from './config/database.js';
 
 dotenv.config();
 
@@ -9,9 +10,8 @@ const app = express();
 app.get('/products', (req, res) => {
 });
 
-console.log(process.env.MONGO_URI);
-
 app.listen(5000, () => {
+    connectDB();
     console.log('Server is running on http://localhost:5000');
 });
 
