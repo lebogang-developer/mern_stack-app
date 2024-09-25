71db97bf435e62a4e707645d9b7335101a78e2cf
Lebogang Sekaleli <lebogangsekaleli@gmail.com>
2024-09-25 19:03:32 +0200

    Backend complete

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/database.js';
-import Product from './models/product.model.js';
+import productsRoutes from './routes/product.route.js';
 
 dotenv.config();
 
@@ -9,26 +9,7 @@ const app = express();
 
 app.use(express.json());
 
-// Post route
-app.post('/api/products', async (req, res) => {
-  const product = req.body;
-
-  if (!product.name || !product.price || !product.image) {
-    return res
-      .status(400)
-      .json({ success: false, message: 'Please provide all required fields!' });
-  }
-
-  const newProduct = new Product(product);
-
-  try {
-    await newProduct.save();
-    res.status(201).json({ success: true, data: newProduct });
-  } catch (error) {
-    console.log('Error in create Product: error.message');
-    res.status(500).json({ success: true, message: 'Server error' });
-  }
-});
+app.use('/api/products/', productsRoutes);
 
 app.listen(5000, () => {
   connectDB();
