e9c3b69d856849e272a1767d099384b5af378480
Lebogang Sekaleli <lebogangsekaleli@gmail.com>
2024-09-25 19:45:33 +0200

    Chakra UI installed

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from '@chakra-ui/react';
+
+function App() {
+  return (
+    <>
+      <h1>Mern App</h1>
+      <Button colorScheme='blue'>Click</Button>
+    </>
+  );
+}
+
+export default App;
